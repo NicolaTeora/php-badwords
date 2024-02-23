e2f3cbdf818475b42602a6d0e7b7e764edb946e5
NicolaTeora <nikxodia@gmail.com>
2024-02-23 20:55:14 +0100

    feat : -enjoy- add style

--- response.php
+++ response.php
@@ -14,6 +14,9 @@ $new_text = str_replace($word , '***', $text);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- my css -->
+    <link rel="stylesheet" href="./style.css">
     
     <!-- cbn Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -22,17 +25,17 @@ $new_text = str_replace($word , '***', $text);
 
 <body>
     <div class="container">
-        
+
         <!-- stampo il contenuto ricevuto tramite input e messo nella variabile php -->
-        <span class="text-decoration-underline">Testo originale:</span>
+        <span class="text-decoration-underline title-span">Testo originale:</span>
         <p><?php  echo $text ?></p>
 
-        <span class="text-decoration-underline">Lunghezza del Testo:</span>
+        <span class="text-decoration-underline title-span">Lunghezza del Testo:</span>
         <!-- stampo la lunghezza dell'array -->
         <p><?php echo $length_p ?></p>
 
         <!-- stampo il testo formattato -->
-        <span class="text-decoration-underline">Testo censurato:</span>
+        <span class="text-decoration-underline title-span">Testo censurato:</span>
         <p><?php echo $new_text ?></p>
     </div>    
 </body>
