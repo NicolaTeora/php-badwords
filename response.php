151223cdf60b16f14a9e0d33d911c78cdfc2d3d2
NicolaTeora <nikxodia@gmail.com>
2024-02-23 20:22:50 +0100

    Feat : dev get and function on string

---
+++ response.php
@@ -0,0 +1,33 @@
+<?php 
+//tramite "get" metto in un variabile il valore mandato tramite input
+$text = $_GET['text_element'];
+$length_p = strlen($text);
+$new_text = str_replace('codice', '***', $text);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <!-- cbn Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+</head>
+
+<body>
+    <div class="container">
+        <p>
+            <!-- stampo il contenuto ricevuto tramite input e messo nella variabile php -->
+            <?php  echo $text ?>
+        </p>
+        <p>
+            <?php echo $new_text ?>
+        </p>
+    </div>    
+</body>
+
+</html>
