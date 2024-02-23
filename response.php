54930f2f646515d5390da742357e084782c298ff
NicolaTeora <nikxodia@gmail.com>
2024-02-23 20:36:46 +0100

    feat: add elements

--- response.php
+++ response.php
@@ -1,8 +1,10 @@
 <?php 
 //tramite "get" metto in un variabile il valore mandato tramite input
 $text = $_GET['text_element'];
+$word = $_GET['one_word'];
+
 $length_p = strlen($text);
-$new_text = str_replace('codice', '***', $text);
+$new_text = str_replace($word , '***', $text);
 
 ?>
 
@@ -20,13 +22,17 @@ $new_text = str_replace('codice', '***', $text);
 
 <body>
     <div class="container">
-        <p>
-            <!-- stampo il contenuto ricevuto tramite input e messo nella variabile php -->
-            <?php  echo $text ?>
-        </p>
-        <p>
-            <?php echo $new_text ?>
-        </p>
+        <!-- stampo il contenuto ricevuto tramite input e messo nella variabile php -->
+        <span>Testo originale:</span>
+        <p><?php  echo $text ?></p>
+
+        <span>Lunghezza del Testo:</span>
+        <!-- stampo la lunghezza dell'array -->
+        <p><?php echo $length_p ?></p>
+
+        <!-- stampo il testo formattato -->
+        <span>Testo censurato:</span>
+        <p><?php echo $new_text ?></p>
     </div>    
 </body>
 
